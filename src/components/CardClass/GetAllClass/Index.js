c3efcb3943666573a2d8f43f4c3fd918807dcde2
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-01-21 18:34:21 +0700

    hapus shadow card

--- src/components/CardClass/GetAllClass/Index.js
+++ src/components/CardClass/GetAllClass/Index.js
@@ -14,7 +14,7 @@ export default async function GettAllClass() {
                         const mentor_name = data.mentor_name ? data.mentor_name.replace(/ /g, '-') : '';
                         return (
                             <Link href={`/${mentor_name}/${data.materi_id}`} key={data.materi_id}>
-                                <div className="card w-96 mt-10 h-5/6 bg-blue-700 text-white shadow-lg shadow-blue-300 md:m-14 md:ml-0 hover:scale-105 duration-300">
+                                <div className="card w-96 mt-10 h-5/6 bg-blue-700 text-white shadow-lg md:m-14 md:ml-0 hover:scale-105 duration-300">
                                     <figure className=' h-60 rounded-2xl'>
                                         <img src={data.image} className=' w-96 h-60 object-cover' alt="Shoes" />
                                     </figure>
