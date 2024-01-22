d3337dc5c220d762955549ad71f268aa344499e9
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-01-22 17:59:47 +0700

    bug deploy

--- src/Redux/features/provider.js
+++ src/Redux/features/provider.js
@@ -5,5 +5,7 @@ import { Provider } from 'react-redux'
 import React from 'react'
 
 export default function ReduxProvider({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>
+        {children}
+    </Provider>
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/Redux/features/provider";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,8 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      {/* <Helmet> */}
-      {/* </Helmet> */}
       <body className={inter.className}>
         <Toaster position="top-center" reverseOrder={false} />
         <ReduxProvider>
@@ -22,8 +21,8 @@ export default function RootLayout({ children }) {
 
       </body>
       {/* <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-fxxfR1HkBON9Jw1t"></script> */}
-      <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key={process.env.MIDTRANS_CLIENT_KEY}></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
+      <Script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key={process.env.MIDTRANS_CLIENT_KEY} />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js" />
     </html>
   );
 }
