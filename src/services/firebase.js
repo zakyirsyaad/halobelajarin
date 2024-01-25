c00256e63eb765aaa98d77afa8cf5b75cd8b155f
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-01-25 15:41:13 +0700

    toast login google ketinggalan

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import toast from "react-hot-toast";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
